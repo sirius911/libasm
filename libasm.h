e8a952b3e098bba3a2461fa03b2f59b05b6f8588
clorin <clorin@student.42.fr>
2021-02-04 14:01:52 +0100

    norm

--- libasm.h
+++ libasm.h
@@ -18,26 +18,23 @@
 # include <stddef.h>
 # include <unistd.h>
 
-typedef struct 		s_list
+typedef struct		s_list
 {
 	void			*data;
 	struct s_list	*next;
 }					t_list;
 
-size_t		ft_strlen(const char *s);
-char		*ft_strcpy(char *dest, const char *src);
-int			ft_strcmp(const char *s1, const char *s2);
-char		*ft_strdup(const char *s);
-ssize_t		ft_read(int fd, void *buf, size_t count);
-ssize_t		ft_write(int fd, void *buf, size_t count);
-
-int			ft_list_size(t_list *begin_list);
-void		ft_list_push_front(t_list **begin_list, void *data);
-void		ft_list_sort(t_list **begin_list, int (*cmp)());
-void		ft_list_remove_if(t_list **begin_list, void *data, int (*cmp)(),
-			void (*free_f)(void *));
-int			ft_atoi_base(char *str, char *base);
-
-void		ft_print_list(t_list **begin_list);
+size_t				ft_strlen(const char *s);
+char				*ft_strcpy(char *dest, const char *src);
+int					ft_strcmp(const char *s1, const char *s2);
+char				*ft_strdup(const char *s);
+ssize_t				ft_read(int fd, void *buf, size_t count);
+ssize_t				ft_write(int fd, void *buf, size_t count);
+int					ft_list_size(t_list *begin_list);
+void				ft_list_push_front(t_list **begin_list, void *data);
+void				ft_list_sort(t_list **begin_list, int (*cmp)());
+void				ft_list_remove_if(t_list **begin_list, void *data,
+					int (*cmp)(), void (*free_f)(void *));
+int					ft_atoi_base(char *str, char *base);
 
 #endif
